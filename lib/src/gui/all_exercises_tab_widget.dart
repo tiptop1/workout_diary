c9b7227026c2d1857e69b40e7f19071ee0212bad
tiptop <Tomasz.Gasior@gmail.com>
2021-10-29 10:45:53 +0200

    Initial commit.

---
+++ lib/src/gui/all_exercises_tab_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:workout_diary/src/repository.dart';
+
+import '../domain.dart';
+
+class AllExercisesTabWidget extends StatefulWidget {
+  @override
+  State<AllExercisesTabWidget> createState() => _AllExercisesState();
+}
+
+class _AllExercisesState extends State<AllExercisesTabWidget> {
+  List<Exercise>? _exercises;
+
+  @override
+  Widget build(BuildContext context) {
+    var widget;
+    if (_exercises == null) {
+      Repository.of(context)
+          .finaAllExerciseSummaries()
+          .then((List<Exercise> exercises) {
+        setState(() {
+          _exercises = exercises;
+        });
+      });
+      widget = CircularProgressIndicator();
+    } else {
+      widget = _build(context);
+    }
+    return widget;
+  }
+
+  Widget _build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: _exercises!.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Text(_exercises![index].name);
+      },
+    );
+  }
+}
---
+++ lib/src/gui/all_workouts_tab_widget.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AllWorkoutsTabWidget extends StatelessWidget {
+  Widget build(BuildContext context) => Text('$this - implement it!');
+}
