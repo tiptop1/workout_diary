ed872b4d8b4d377b86d598e004fe5b42ba5d3565
tiptop <Tomasz.Gasior@gmail.com>
2021-10-29 13:25:34 +0200

    Create AppMaterial widget only once - at higher widget tree level.

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,28 @@
-import 'package:async/async.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:workout_diary/src/config.dart';
-import 'package:workout_diary/src/domain.dart';
-import 'package:workout_diary/src/gui/workout_diary_widget.dart';
-import 'package:workout_diary/src/repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'src/gui/all_workouts_tab_widget.dart';
+import 'src/gui/app_initialization_widget.dart';
 
 void main() {
   // Don't know how, but according to documentation:
   // "Avoid errors caused by flutter upgrade".
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(_InitializationWidget());
+  runApp(MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    theme: ThemeData.light(),
+    darkTheme: ThemeData.dark(),
+    onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
+    initialRoute: Route.appInitialization,
+    routes: {
+      Route.appInitialization: (context) => AppInitializationWidget(),
+      Route.allWorkouts: (context) => AllWorkoutsTabWidget(),
+    },
+  ));
 }
 
-class _InitializationWidget extends StatefulWidget {
-  @override
-  State<_InitializationWidget> createState() => _InitializationWidgetState();
-}
-
-class _InitializationWidgetState extends State<_InitializationWidget> {
-  SharedPreferences? _sharedPreferences;
-  Database? _database;
-
-  @override
-  void initState() {
-    FutureGroup futures = FutureGroup();
-    futures.add(SharedPreferences.getInstance());
-    futures.add(_openDatabase());
-    futures.close();
-    futures.future.then(
-      (results) {
-        setState(
-          () {
-            _sharedPreferences = results[0];
-            _database = results[1];
-          },
-        );
-      },
-    );
-    super.initState();
-  }
-
-  @override
-  void dispose() async {
-    _database?.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext ctx) {
-    Widget widget;
-    if (_initializationComplete()) {
-      widget = Configuration(
-        sharedPreferences: _sharedPreferences!,
-        child: Repository(database: _database!, child: WorkoutDiaryWidget()),
-      );
-    } else {
-      widget = CircularProgressIndicator();
-    }
-    return widget;
-  }
-
-  // Usually the _database will be completed.
-  // Use it creating new Future e.g. _database.then(Future(...))
-  Future<Database> _openDatabase() async {
-    return openDatabase(
-      join(await getDatabasesPath(), 'workout_diary.db'),
-      onCreate: (db, version) {
-        db.execute(
-          'CREATE TABLE ${Exercise.table}(${Exercise.colId} INTEGER PRIMARY KEY AUTOINCREMENT, ${Exercise.colName} TEXT NOT NULL, ${Exercise.colDescription} TEXT)',
-        );
-        db.execute(
-            'CREATE TABLE ${WorkoutEntry.table}(${WorkoutEntry.colId} INTEGER PRIMARY KEY AUTOINCREMENT, ${WorkoutEntry.colExerciseId} INTEGER NOT NULL, ${WorkoutEntry.colWorkoutId} INTEGER NOT NULL, ${WorkoutEntry.colDetails} TEXT, FOREIGN KEY (${WorkoutEntry.colExerciseId}) REFERENCES ${Exercise.table}(${Exercise.colId}), FOREIGN KEY (${WorkoutEntry.colWorkoutId}) REFERENCES ${Workout.table}(${Workout.colId}))');
-        db.execute(
-            'CREATE TABLE ${Workout.table}(${Workout.colId} INTEGER PRIMARY KEY AUTOINCREMENT, ${Workout.colStartTime} INTEGER NOT NULL, ${Workout.colEndTime} INTEGER NOT NULL, ${Workout.colTitle} TEXT, ${Workout.colPreComment} TEXT, ${Workout.colPostComment} TEXT)');
-      },
-      version: 1,
-    );
-  }
-
-  bool _initializationComplete() {
-    return _database != null && _sharedPreferences != null;
-  }
+class Route {
+  static const appInitialization = '/appInitialization';
+  static const allWorkouts = '/allWorkouts';
 }
--- lib/src/gui/workout_diary_widget.dart
+++ lib/src/gui/workout_diary_widget.dart
@@ -2,55 +2,37 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'all_exercises_tab_widget.dart';
 import 'all_workouts_tab_widget.dart';
-
-class Route {
-  static const allWorkouts = '/allWorkouts';
-}
 
 class WorkoutDiaryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
-      initialRoute: Route.allWorkouts,
-      routes: {
-        Route.allWorkouts: (context) => _buildAllWorkoutsTabWidget(context),
-      },
+    var tabs = {
+      AppLocalizations.of(context)!.workoutsTab: AllWorkoutsTabWidget(),
+      AppLocalizations.of(context)!.exercisesTab: AllExercisesTabWidget(),
+    } as LinkedHashMap;
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [...tabs.keys.map((e) => Tab(text: e))],
+          ),
+          title: Text(AppLocalizations.of(context)!.appTitle),
+        ),
+        body: TabBarView(
+          children: [...tabs.values],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
-
-Widget _buildAllWorkoutsTabWidget(BuildContext context) {
-  var tabs = {
-    AppLocalizations.of(context)!.workoutsTab: AllWorkoutsTabWidget(),
-    AppLocalizations.of(context)!.exercisesTab: AllExercisesTabWidget(),
-  } as LinkedHashMap;
-
-  return DefaultTabController(
-    length: tabs.length,
-    child: Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          tabs: [...tabs.keys.map((e) => Tab(text: e))],
-        ),
-        title: Text(AppLocalizations.of(context)!.appTitle),
-      ),
-      body: TabBarView(
-        children: [...tabs.values],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: const Icon(Icons.add),
-      ),
-    ),
-  );
-}
-
