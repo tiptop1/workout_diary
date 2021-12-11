984dadd331065f3d7911d693c2700f50654a4aed
tiptop <Tomasz.Gasior@gmail.com>
2021-12-11 23:52:42 +0100

    Add PopupMenuButton on trailing menu on exercise list.

--- lib/src/gui/all_exercises_tab_widget.dart
+++ lib/src/gui/all_exercises_tab_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:workout_diary/src/config.dart';
 
 import '../domain.dart';
 import '../repository.dart';
 import 'exercise_widgets.dart';
 import 'progress_widget.dart';
+
+enum ExerciseAction { modify, delete }
 
 class AllExercisesTabWidget extends StatefulWidget {
   AllExercisesTabWidget({Key? key}) : super(key: key);
@@ -35,6 +38,7 @@ class _AllExercisesState extends State<AllExercisesTabWidget> {
   }
 
   Widget _build(BuildContext context) {
+    AppLocalizations appLocalizations = AppLocalizations.of(context)!;
     return ListView.builder(
       padding: const EdgeInsets.all(8),
       itemCount: _exercises!.length,
@@ -42,7 +46,28 @@ class _AllExercisesState extends State<AllExercisesTabWidget> {
         return Card(
           child: ListTile(
             title: Text(_exercises![index].name),
-            trailing: Icon(Icons.menu_rounded),
+            // trailing: Icon(Icons.menu_rounded),
+            trailing: PopupMenuButton<ExerciseAction>(
+              icon: Icon(Icons.menu_rounded),
+              onSelected: (ExerciseAction result) {
+                if (result == ExerciseAction.modify) {
+
+                } else if (result == ExerciseAction.delete) {
+
+                }
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<ExerciseAction>>[
+                PopupMenuItem<ExerciseAction>(
+                  value: ExerciseAction.modify,
+                  child: Text(appLocalizations.modify),
+                ),
+                PopupMenuItem<ExerciseAction>(
+                  value: ExerciseAction.delete,
+                  child: Text(appLocalizations.delete),
+                ),
+              ],
+            ),
             onTap: () {
               var sharedPrefs = Configuration.of(context).sharedPreferences;
               var db = Repository.of(context).database;
