89c397bf3204a636eae8e0bdd10c2fe491e035bc
tiptop <Tomasz.Gasior@gmail.com>
2022-03-04 15:23:59 +0100

    Implement AddWorkoutWidget - work in progress.

--- lib/src/gui/workout_widgets.dart
+++ lib/src/gui/workout_widgets.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
-
-import '../domain.dart';
-import '../gui/progress_widget.dart';
+import 'package:tuple/tuple.dart';
+import 'package:workout_diary/src/domain.dart';
+import 'package:workout_diary/src/gui/progress_widget.dart';
+import 'package:workout_diary/src/repository.dart';
 
 class AddWorkoutWidget extends StatefulWidget {
   @override
   State<AddWorkoutWidget> createState() => _AddWorkoutState();
 }
 
+// TODO: Ensure that at least one Exercise has been added to database.
 class _AddWorkoutState extends State<AddWorkoutWidget> {
   static const String dateLackMarker = '-';
 
-  List<Workout>? _workouts;
-
+  bool _inProgress = false;
+  DateTime? _startTime;
+  DateTime? _endTime;
+  late TextEditingController _titleController;
   late TextEditingController _preCommentController;
   late TextEditingController _postCommentController;
-  late List<TextEditingController> _entryControllers;
+  late List<Tuple2<Exercise, TextEditingController>> _entryTuples;
+  late List<Exercise>? _exercises;
 
   @override
   void initState() {
     super.initState();
+    _titleController = TextEditingController();
     _preCommentController = TextEditingController();
     _postCommentController = TextEditingController();
-    _entryControllers = [];
+    _entryTuples = [];
   }
 
   @override
   void dispose() {
     _postCommentController.dispose();
     _preCommentController.dispose();
-    _entryControllers.forEach((c) => c.dispose());
+    _entryTuples.forEach((t) => t.item2.dispose());
+    _titleController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var widget;
-    if (_workouts == null) {
-      widget = createAddWorkoutWidget(context);
-    } else {
-      widget = ProgressWidget();
-    }
-    return widget;
-  }
-
-  Widget createAddWorkoutWidget(BuildContext context) {
     var appLocalizations = AppLocalizations.of(context)!;
-
-    return Scaffold(
+    var widget;
+    if (_exercises == null) {
+      _inProgress = true;
+      Repository.of(context).findAllExerciseSummaries().then((exercises) {
+        setState(() {
+          _exercises = exercises;
+          _inProgress = false;
+        });
+      });
+    }
+    if (_inProgress) {
+      widget = ProgressWidget();
+    } else {
+      widget = Scaffold(
         appBar: AppBar(
           title: Text(appLocalizations.addWorkoutTitle),
           leading: IconButton(
@@ -60,12 +69,35 @@ class _AddWorkoutState extends State<AddWorkoutWidget> {
         ),
         body: Column(
           children: [
-            _createTimeBar(context, appLocalizations, null, null),
+            _createTitleWidget(context, appLocalizations),
+            _createTimeBar(context, appLocalizations, _startTime, _endTime),
             _createPrecommentWidget(context, appLocalizations),
-            _createWorkoutEntries(context),
+            _createWorkoutEntryWidget(context),
             _createPostcommentWidget(context, appLocalizations),
           ],
-        ));
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _entryTuples
+                    .add(Tuple2(_exercises!.first, TextEditingController()));
+              });
+            },
+            child: const Icon(Icons.add)),
+      );
+    }
+    return widget;
+  }
+
+  Widget _createTitleWidget(
+      BuildContext context, AppLocalizations appLocalizations) {
+    return TextField(
+      controller: _titleController,
+      decoration: InputDecoration(
+          labelText: appLocalizations.workoutTitle,
+          border: OutlineInputBorder(),
+          hintText: appLocalizations.workoutTitleHint),
+    );
   }
 
   Widget _createTimeBar(BuildContext context, AppLocalizations appLocalizations,
@@ -78,7 +110,20 @@ class _AddWorkoutState extends State<AddWorkoutWidget> {
       children: [
         Text(
             '${appLocalizations.start}: $startStr, ${appLocalizations.end}: $endStr'),
-        // TODO: Add buttons to control time
+        _createTimeControlButton(
+          icon: Icons.play_arrow,
+          isEnabled:
+              (start != null && end != null) || (start == null && end == null),
+          onPress: () {
+            _startTime = DateTime.now();
+            _endTime = null;
+          },
+        ),
+        _createTimeControlButton(
+          icon: Icons.stop,
+          isEnabled: start != null,
+          onPress: () => _endTime = DateTime.now(),
+        ),
       ],
     );
   }
@@ -86,11 +131,32 @@ class _AddWorkoutState extends State<AddWorkoutWidget> {
   String _dateTimeStr(Locale locale, DateTime dateTime) =>
       '${DateFormat.yMMMMd(locale).format(dateTime)} ${DateFormat.jm(locale).format(dateTime)}';
 
-  void _backButtonCallback(BuildContext context) =>
-      throw UnsupportedError('Not implemented yet!');
+  void _backButtonCallback(BuildContext context) {
+    var workout = Workout(
+        startTime: _startTime,
+        endTime: _endTime,
+        title: _titleController.value.text,
+        preComment: _preCommentController.value.text,
+        postComment: _postCommentController.value.text);
+    var repo = Repository.of(context);
+    repo.insertWorkout(workout).then((insertedWorkout) {
+      repo
+          .insertAllWorkoutEntries(
+              _createWorkoutEntriesList(insertedWorkout, _entryTuples))
+          .then((insertedEntriesList) {
+        Navigator.pop(context, true);
+      });
+      // TODO: Add error handling for workout entries insert.
+    });
+    // TODO: Add error handling for workout insert.
+    setState(() => _inProgress = true);
+  }
 
-  Widget _createWorkoutEntries(BuildContext context) =>
-      throw UnsupportedError('Not implemented yet!');
+  Widget _createWorkoutEntryWidget(BuildContext context) {
+    return ListView(children: [
+      for (var t in _entryTuples) _createWorkoutEntryListTile(t)
+    ],);
+  }
 
   Widget _createPrecommentWidget(
       BuildContext context, AppLocalizations appLocalizations) {
@@ -114,5 +180,28 @@ class _AddWorkoutState extends State<AddWorkoutWidget> {
           border: OutlineInputBorder(),
           hintText: appLocalizations.workoutPostcommentHint),
     );
+  }
+
+  Widget _createTimeControlButton(
+      {required IconData icon,
+      required bool isEnabled,
+      required VoidCallback onPress}) {
+    return ElevatedButton(
+        onPressed: isEnabled ? onPress : null, child: Icon(icon));
+  }
+
+  List<WorkoutEntry> _createWorkoutEntriesList(Workout workout,
+      List<Tuple2<Exercise, TextEditingController>> entryTuples) {
+    var entries = <WorkoutEntry>[];
+    _entryTuples.forEach((t) => entries.add(WorkoutEntry(
+          exercise: t.item1,
+          workout: workout,
+          details: t.item2.value.text,
+        )));
+    return entries;
+  }
+
+  Widget _createWorkoutEntryListTile(Tuple2<Exercise, TextEditingController> tuple2) {
+    throw UnimplementedError("Not implemented yet!");
   }
 }
