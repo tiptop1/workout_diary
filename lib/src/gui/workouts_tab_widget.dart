cb8a356ba042b2048614aa57f4c60caaa8c93173
tiptop <Tomasz.Gasior@gmail.com>
2022-03-25 14:48:39 +0100

    Add implementation of AllWorkoutsTabWidget.

--- lib/src/gui/workouts_tab_widget.dart
+++ lib/src/gui/workouts_tab_widget.dart
@@ -1,5 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class AllWorkoutsTabWidget extends StatelessWidget {
-  Widget build(BuildContext context) => Text('$this - implement it!');
+import 'package:flutter/material.dart';
+import 'package:workout_diary/src/gui/list_tab_widget.dart';
+import 'package:workout_diary/src/gui/workout_widgets.dart';
+
+import '../domain.dart';
+import '../repository.dart';
+
+class AllWorkoutsTabWidget extends ListOnTabWidget {
+  AllWorkoutsTabWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AllWorkoutsTabWidget> createState() => _AllWorkoutsState();
+}
+
+class _AllWorkoutsState extends ListOnTabState<AllWorkoutsTabWidget, Workout> {
+  @override
+  void loadEntities(BuildContext context) {
+    Repository.of(context)
+        .findAllWorkoutSummaries()
+        .then((List<Workout> workouts) {
+      setState(() {
+        entities = workouts;
+        entitiesReady = true;
+      });
+    }).catchError((error) {
+      log('Can not load exercises!');
+    });
+  }
+
+  Widget listItemTitle(BuildContext context, Workout workout) {
+    var startTime = workout.startTime;
+    var endTime = workout.endTime;
+    var locale = Localizations.localeOf(context);
+    var startTimeStr =
+        startTime != null ? dateTimeStr(locale, startTime) : dateLackMarker;
+    var endTimeStr =
+        endTime != null ? dateTimeStr(locale, endTime) : dateLackMarker;
+    return Text('$startTimeStr $endTimeStr');
+  }
+
+  void listItemModifyAction(BuildContext context, Workout workout) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Workout modify action not implemented yet!'),
+    ));
+  }
+
+  void listItemDeleteAction(BuildContext context, Workout workout) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Workout delete action not implemented yet!'),
+    ));
+  }
+
+  void listItemShowAction(BuildContext context, Workout workout) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Workout show action not implemented yet!'),
+    ));
+  }
 }
