b94075f9ed50e92857b695c6f7ffaff8e4595b52
tiptop <Tomasz.Gasior@gmail.com>
2022-06-03 11:04:00 +0200

    Reorganize widget tree - first add AppInitializationWidget (it add inherited widgets) and then WorkoutDiaryWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:workout_diary/src/gui/workout_diary_widget.dart';
 
 import 'src/gui/app_initialization_widget.dart';
 
@@ -7,11 +8,13 @@ void main() {
   // Don't know how, but according to documentation:
   // "Avoid errors caused by flutter upgrade".
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
-      home: AppInitializationWidget()));
+  runApp(AppInitializationWidget(
+      child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          onGenerateTitle: (BuildContext ctx) =>
+              AppLocalizations.of(ctx)!.appTitle,
+          home: WorkoutDiaryWidget())));
 }
