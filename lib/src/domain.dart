cb44271a898caf255a07b91078ef466aeda4cf48
tiptop <Tomasz.Gasior@gmail.com>
2022-06-10 14:53:07 +0200

    Add to entries fromJson() constructor and toJson() method.

--- lib/src/domain.dart
+++ lib/src/domain.dart
@@ -1,10 +1,19 @@
 abstract class Entity {
+  static const idField = 'id';
+
   final int? id;
 
   const Entity({this.id});
+
+  Entity.fromJson(Map<String, dynamic> json) : id = json[idField];
+
+  Map<String, dynamic> toJson() => {idField: id};
 }
 
 class Exercise extends Entity {
+  static const nameField = 'name';
+  static const descriptionField = 'description';
+
   final String name;
   final String? description;
 
@@ -12,36 +21,68 @@ class Exercise extends Entity {
       : assert(name.isNotEmpty),
         super(id: id);
 
+  Exercise.fromJson(Map<String, dynamic> json)
+      : this(
+            id: json[Entity.idField],
+            name: json[nameField],
+            description: json[descriptionField]);
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = super.toJson();
+    json[nameField] = name;
+    json[descriptionField] = description;
+    return json;
+  }
+
   @override
   String toString() {
-    return 'Exercise{id: $id, name: $name, description: $description}';
+    return toJson().toString();
   }
 }
 
 class WorkoutEntry extends Entity {
+  static const exerciseField = 'exercise';
+  static const detailsField = 'details';
+
   final Exercise exercise;
-  final Workout workout;
   final String details;
 
-  const WorkoutEntry(
-      {int? id,
-      required this.exercise,
-      required this.workout,
-      required this.details})
+  const WorkoutEntry({int? id, required this.exercise, required this.details})
       : super(id: id);
+
+  WorkoutEntry.fromJson(Map<String, dynamic> json)
+      : this(
+            id: json[Entity.idField],
+            exercise: Exercise.fromJson(json),
+            details: json[detailsField]);
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = super.toJson();
+    json[exerciseField] = exercise.toJson();
+    json[detailsField] = details;
+    return json;
+  }
 
   @override
   String toString() {
-    return 'WorkoutEntry{id: $id, exercise: ${exercise.toString()}, workout: ${workout.toString()}, details: $details}';
+    return toJson().toString();
   }
 }
 
 class Workout extends Entity {
+  static const startTimeField = 'startTime';
+  static const endTimeField = 'endTime';
+  static const titleField = 'title';
+  static const preCommentField = 'preComment';
+  static const postCommentField = 'postComment';
+  static const entitiesField = 'entities';
+
   final DateTime? startTime;
   final DateTime? endTime;
   final String title;
   final String? preComment;
   final String? postComment;
+  final List<WorkoutEntry> _entities = [];
 
   Workout(
       {int? id,
@@ -53,8 +94,35 @@ class Workout extends Entity {
       : assert(title.isNotEmpty),
         super(id: id);
 
+  Workout.formJson(Map<String, dynamic> json)
+      : this(
+            id: json[Entity.idField],
+            startTime: json[startTimeField],
+            endTime: json[endTimeField],
+            title: json[titleField],
+            preComment: json[preCommentField],
+            postComment: json[postCommentField]);
+
+  List<WorkoutEntry> get entities => List.unmodifiable(_entities);
+
+  void addWorkoutEntry(WorkoutEntry entry) => _entities.add(entry);
+
+  void removeWorkoutEntry(WorkoutEntry entry) =>
+      _entities.remove(_entities.firstWhere((e) => entry.id == e.id));
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = super.toJson();
+    json[startTimeField] = startTime;
+    json[endTimeField] = endTime;
+    json[titleField] = title;
+    json[preCommentField] = preComment;
+    json[postCommentField] = postComment;
+    json[entitiesField] = _entities;
+    return json;
+  }
+
   @override
   String toString() {
-    return 'Workout{id: $id, startTime: $startTime, endTime: $endTime, title: $title, preComment: $preComment, postComment: $postComment}';
+    return toJson().toString();
   }
 }
