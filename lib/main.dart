7a2a803ca1b42fc73470d849e64660b1701b344f
tiptop <Tomasz.Gasior@gmail.com>
2022-08-26 16:00:03 +0200

    Use GetIt service provider. Use FutureBuilder to show ProgressWidget in case of not initialized application yet.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:workout_diary/src/gui/progress_widget.dart';
 import 'package:workout_diary/src/gui/workout_diary_widget.dart';
 
-import 'src/gui/app_initialization_widget.dart';
+import 'src/model/repository.dart';
 
 void main() {
-  // Don't know how, but according to documentation:
-  // "Avoid errors caused by flutter upgrade".
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(AppInitializationWidget(
-      child: MaterialApp(
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          onGenerateTitle: (BuildContext ctx) =>
-              AppLocalizations.of(ctx)!.appTitle,
-          home: WorkoutDiaryWidget())));
+  GetIt.I.registerSingletonAsync<Repository>(
+    () async => Repository.init(),
+    // TODO: Make sure, that GetIt dispose method is run implicit after application finish
+    dispose: (repository) => repository.dispose(),
+  );
+
+  runApp(MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    theme: ThemeData.light(),
+    darkTheme: ThemeData.dark(),
+    onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
+    home: buildWidget(),
+  ));
+}
+
+Widget buildWidget() {
+  return FutureBuilder(
+    future: GetIt.I.allReady(),
+    builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return snapshot.hasData ? WorkoutDiaryWidget() : ProgressWidget();
+    },
+  );
 }
