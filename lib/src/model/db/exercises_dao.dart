c7faffe3d34ad4a5a40e9324a8e3c4dbc00b4b38
tiptop <Tomasz.Gasior@gmail.com>
2022-08-26 15:58:36 +0200

    Remove unnecessary result code checking.

--- lib/src/model/db/exercises_dao.dart
+++ lib/src/model/db/exercises_dao.dart
@@ -1,5 +1,4 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:workout_diary/src/model/db/sqlite_result_codes.dart';
 
 import '../exercise.dart';
 
@@ -57,28 +56,20 @@ class ExercisesDao {
       },
       conflictAlgorithm: ConflictAlgorithm.rollback,
     );
-    if (id == SqliteResultCodes.sqliteConstraint.code) {
-      throw Exception('Could not insert exercise - result code: $id.');
-    } else {
-      return Exercise(
-        id: id,
-        name: name,
-        description: description,
-      );
-    }
+    return Exercise(
+      id: id,
+      name: name,
+      description: description,
+    );
   }
 
-  /// Update [Excrcise] in repository.
-  /// Returns count of updated exercise records.
-  Future<Exercise> update(Exercise exercise, Transaction txn) async {
-    var resultCode = await txn.update(
+  Future<Exercise?> update(Exercise exercise, Transaction txn) async {
+    var updatedRecordsCount = await txn.update(
         table, {colName: exercise.name, colDescription: exercise.description},
-        where: '$colId = ?', whereArgs: [exercise.id], conflictAlgorithm: ConflictAlgorithm.rollback);
-    if (resultCode != 1) {
-      throw Exception('Could not update exercise with id:${exercise.id} - result code: $resultCode.');
-    } else {
-      return exercise;
-    }
+        where: '$colId = ?',
+        whereArgs: [exercise.id],
+        conflictAlgorithm: ConflictAlgorithm.rollback);
+    return updatedRecordsCount == 1 ? exercise : null;
   }
 
   /// Delete [Exercise] with given [id].
