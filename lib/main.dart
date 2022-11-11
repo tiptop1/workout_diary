1ab88f06a10ce94eb9af425bbb1fe35b1e66ec50
tiptop <Tomasz.Gasior@gmail.com>
2022-11-11 09:56:38 +0100

    Remove GetIt calls.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:get_it/get_it.dart';
-import 'package:workout_diary/src/gui/progress_widget.dart';
+import 'package:workout_diary/src/gui/redux_store_init_widget.dart';
 import 'package:workout_diary/src/gui/workout_diary_widget.dart';
 
-import 'src/model/repository.dart';
-
 void main() {
-  GetIt.I.registerSingletonAsync<Repository>(
-    () async => Repository.init(),
-    // TODO: Make sure, that GetIt dispose method is run implicit after application finish
-    dispose: (repository) => repository.dispose(),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MaterialApp(
-    localizationsDelegates: AppLocalizations.localizationsDelegates,
-    supportedLocales: AppLocalizations.supportedLocales,
-    theme: ThemeData.light(),
-    darkTheme: ThemeData.dark(),
-    onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
-    home: buildWidget(),
+  runApp(ReduxStoreInitWidget(
+    child: MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      onGenerateTitle: (BuildContext ctx) => AppLocalizations.of(ctx)!.appTitle,
+      home: WorkoutDiaryWidget(),
+    ),
   ));
-}
-
-Widget buildWidget() {
-  return FutureBuilder(
-    future: GetIt.I.allReady(),
-    builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return snapshot.hasData ? WorkoutDiaryWidget() : ProgressWidget();
-    },
-  );
 }
