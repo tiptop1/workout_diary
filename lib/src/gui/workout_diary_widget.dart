5516f1e9223816504b94a53819464b8855a60f25
t.gasior <t.gasior@crif.com>
2022-12-31 00:35:41 +0100

    Add StoreConnector.

--- lib/src/gui/workout_diary_widget.dart
+++ lib/src/gui/workout_diary_widget.dart
@@ -2,7 +2,10 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
+import '../model/app_state.dart';
+import '../model/exercise.dart';
 import 'exercise_widget.dart';
 import 'exercises_tab_widget.dart';
 import 'workout_widgets.dart';
@@ -36,42 +39,72 @@ class _WorkoutDiaryWidgetState extends State<WorkoutDiaryWidget>
     var tabs = {
       appLocalizations.workoutsTab: WorkoutsTabWidget(key: UniqueKey()),
       appLocalizations.exercisesTab: ExercisesTabWidget(key: UniqueKey()),
-    } as LinkedHashMap;
+    } as LinkedHashMap<String, Widget>;
 
-    return Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: [...tabs.keys.map((e) => Tab(text: e))],
-        ),
-        title: Text(AppLocalizations.of(context)!.appTitle),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [...tabs.values],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          var currentTab = tabs.keys.elementAt(_tabController.index);
-          var tabWidget;
-          if (currentTab == appLocalizations.workoutsTab) {
-            tabWidget = WorkoutWidget();
-          } else if (currentTab == appLocalizations.exercisesTab) {
-            tabWidget = ExerciseWidget(
-              key: UniqueKey(),
-              modifiable: true,
-            );
-          } else {
-            assert(false, 'Tab $currentTab not supported.');
-          }
-          Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => tabWidget))
-              .then((_) {
-            setState(() {});
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
+    return StoreConnector<AppState, List<Exercise>>(
+      onInitialBuild: (exercises) => _tabController.index = exercises.isEmpty
+          ? _findIndex(tabs, appLocalizations.exercisesTab)
+          : 0,
+      onWillChange: (prevExercises, exercises) {
+        if (exercises.isEmpty) {
+          _tabController.index =
+              _findIndex(tabs, appLocalizations.exercisesTab);
+        }
+      },
+      converter: (store) => store.state.exercises,
+      builder: (context, exercises) {
+        return Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              controller: _tabController,
+              tabs: [...tabs.keys.map((e) => Tab(text: e))],
+            ),
+            title: Text(AppLocalizations.of(context)!.appTitle),
+          ),
+          body: TabBarView(
+            controller: _tabController,
+            children: [...tabs.values],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              var currentTab = tabs.keys.elementAt(_tabController.index);
+              var tabWidget;
+              if (currentTab == appLocalizations.workoutsTab) {
+                tabWidget = WorkoutWidget();
+              } else if (currentTab == appLocalizations.exercisesTab) {
+                tabWidget = ExerciseWidget(
+                  key: UniqueKey(),
+                  modifiable: true,
+                );
+              } else {
+                assert(false, 'Tab $currentTab not supported.');
+              }
+              Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => tabWidget))
+                  .then((action) {
+                if (action != null) {
+                  StoreProvider.of<AppState>(context).dispatch(action);
+                }
+              });
+            },
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
+  }
+
+  /// Find index of given [key] - make sense only for ordered [map]'s.
+  int _findIndex(LinkedHashMap<String, dynamic> map, String key) {
+    var i = 0;
+    var indexFound = false;
+    for (var k in map.keys) {
+      if (k == key) {
+        indexFound = true;
+        break;
+      }
+      i++;
+    }
+    return indexFound ? i : 0;
   }
 }
