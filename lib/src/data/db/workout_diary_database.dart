00a1fc8d8c94f61a1488c8888504f01d5c9e7573
tiptop <Tomasz.Gasior@gmail.com>
2023-12-04 23:37:49 +0100

    Rename AppDatabase class to WorkoutDiaryDatabase. Rename some "floor" library stuff (e.g. DAOs, entities).

--- lib/src/data/db/workout_diary_database.dart
+++ lib/src/data/db/workout_diary_database.dart
@@ -1,25 +1,25 @@
-// database.dart
-
 // required package imports
 import 'dart:async';
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
 import 'converter/datetime_converter.dart';
-import 'entity/exercise.dart';
-import 'entity/exerciseset.dart';
-import 'entity/workout.dart';
 
 import 'dao/exercise_dao.dart';
-import 'dao/exerciseset_dao.dart';
+import 'dao/exercise_set_dao.dart';
 import 'dao/workout_dao.dart';
+import 'model/exercise_model.dart';
+import 'model/exercise_set_model.dart';
+import 'model/workout_model.dart';
 
-part 'app_database.g.dart'; // the generated code will be there
+part 'workout_diary_database.g.dart'; // the generated code will be there
 
 @TypeConverters([DateTimeConverter, NullableDateTimeConverter])
-@Database(version: 1, entities: [Exercise, ExerciseSet, Workout])
-abstract class AppDatabase extends FloorDatabase {
+@Database(version: 1, entities: [ExerciseModel, ExerciseSetModel, WorkoutModel])
+abstract class WorkoutDiaryDatabase extends FloorDatabase {
   ExerciseDao get exerciseDao;
+
   ExerciseSetDao get exerciseSetDao;
+
   WorkoutDao get workoutDao;
 }
