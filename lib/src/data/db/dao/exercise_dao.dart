e5a29446ae36896fa6de9a3e43e028cd35c6ffe4
tiptop <Tomasz.Gasior@gmail.com>
2023-12-04 23:28:22 +0100

    Rename database base entity Exercise to ExerciseModel. Add indExerciseByName(String) method.

--- lib/src/data/db/dao/exercise_dao.dart
+++ lib/src/data/db/dao/exercise_dao.dart
@@ -1,18 +1,21 @@
 import 'package:floor/floor.dart';
-import '../entity/exercise.dart';
+import '../model/exercise_model.dart';
 
 @dao
 abstract class ExerciseDao {
-  @Query('SELECT * FROM Exercise')
-  Future<List<Exercise>> findAllExercises();
+  @Query('SELECT * FROM ExerciseModel ORDER BY name')
+  Future<List<ExerciseModel>> findAllExercises();
+
+  /// Find [ExerciseModel] by natural key [name].
+  @Query('SELECT * FROM ExerciseModel WHERE name = :name')
+  Future<ExerciseModel?> findExerciseByName(String name);
 
   @insert
-  Future<int> insertExercise(Exercise exercise);
+  Future<int> insertExercise(ExerciseModel exercise);
 
   @update
-  Future<void> updateExercise(Exercise exercise);
+  Future<void> updateExercise(ExerciseModel exercise);
 
   @delete
-  Future<void> deleteExercise(Exercise exercise);
-
+  Future<void> deleteExercise(ExerciseModel exercise);
 }
