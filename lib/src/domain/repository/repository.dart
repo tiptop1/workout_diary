dc61adfe32b2b3f97b955c711f028e6159bdeeca
tiptop <Tomasz.Gasior@gmail.com>
2023-12-04 23:41:54 +0100

    Rename method getExerciseDetails(String) to getExercise(String). Rename method getWorkoutDetails(String, DateTime) to getWorkout(DateTime). Change list of parameters for method removeWorkout(String, DateTime) to removeWorkout(DateTime).

--- lib/src/domain/repository/repository.dart
+++ lib/src/domain/repository/repository.dart
@@ -7,22 +7,21 @@ import '../entity/workout.dart';
 abstract class Repository {
   Future<Either<Failure, List<Exercise>>> getAllExercises();
 
+  Future<Either<Failure, Exercise>> getExercise(String name);
+
   Future<Either<Failure, void>> addExercise(Exercise exercise);
 
   Future<Either<Failure, void>> removeExercise(String name);
 
   Future<Either<Failure, void>> modifyExercise(Exercise exercise);
 
-  Future<Either<Failure, Exercise>> getExerciseDetails(String name);
-
   Future<Either<Failure, List<Workout>>> getAllWorkouts();
+
+  Future<Either<Failure, Workout>> getWorkout(DateTime startTime);
 
   Future<Either<Failure, void>> addWorkout(Workout workout);
 
-  Future<Either<Failure, void>> removeWorkout(String title, DateTime startTime);
+  Future<Either<Failure, void>> removeWorkout(DateTime startTime);
 
   Future<Either<Failure, void>> modifyWorkout(Workout workout);
-
-  Future<Either<Failure, Workout>> getWorkoutDetails(
-      String title, DateTime startTime);
 }
