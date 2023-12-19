3ccb7cf946af42032b648fb33eea9f96135d55a2
tiptop <Tomasz.Gasior@gmail.com>
2023-12-19 22:39:07 +0100

    Remove index on natural keys. Make id required.

--- lib/src/data/db/model/exercise_set_model.dart
+++ lib/src/data/db/model/exercise_set_model.dart
@@ -4,14 +4,10 @@ import 'package:floor/floor.dart';
 import 'exercise_model.dart';
 import 'workout_model.dart';
 
-/// The entity is syntetic - there is no natural key
 @Entity(tableName: 'exercise_sets')
-@Index(
-    name: 'UX_ExerciseSet_exercise_workout_orderNumber',
-    value: ['exerciseId', 'workoutId', 'orderNumber'])
 class ExerciseSetModel extends Equatable {
-  @PrimaryKey(autoGenerate: true)
-  final int? id;
+  @primaryKey
+  final int id;
   @ForeignKey(
     childColumns: ['exerciseId'],
     parentColumns: ['id'],
@@ -19,21 +15,23 @@ class ExerciseSetModel extends Equatable {
   )
   final int exerciseId;
   @ForeignKey(
-      childColumns: ['workoutId'],
-      parentColumns: ['id'],
-      entity: WorkoutModel,
-      onDelete: ForeignKeyAction.cascade)
+    childColumns: ['workoutId'],
+    parentColumns: ['id'],
+    entity: WorkoutModel,
+    onDelete: ForeignKeyAction.cascade,
+  )
   final int workoutId;
   final int orderNumber;
   final String? details;
 
-  const ExerciseSetModel(
-      {this.id,
-      required this.exerciseId,
-      required this.workoutId,
-      required this.orderNumber,
-      this.details,});
+  const ExerciseSetModel({
+    required this.id,
+    required this.exerciseId,
+    required this.workoutId,
+    required this.orderNumber,
+    this.details,
+  });
 
   @override
-  List<Object?> get props => [id, exerciseId, workoutId, orderNumber];
+  List<Object?> get props => [id];
 }
