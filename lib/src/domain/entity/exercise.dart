5ae876660939953e5d0cac8f1e7f834b090b1339
tiptop <Tomasz.Gasior@gmail.com>
2023-12-19 22:25:54 +0100

    Add synthetic key ExerciseId.

--- lib/src/domain/entity/exercise.dart
+++ lib/src/domain/entity/exercise.dart
@@ -1,13 +1,30 @@
 import 'package:equatable/equatable.dart';
 
+class ExerciseId extends Equatable {
+  final int id;
+
+  const ExerciseId(this.id);
+
+  @override
+  List<Object> get props => [id];
+
+  @override
+  bool get stringify => true;
+}
+
 class Exercise extends Equatable {
+  final ExerciseId id;
   final String name;
   final String? description;
 
-  const Exercise({required this.name, this.description});
+  const Exercise({
+    required this.id,
+    required this.name,
+    this.description,
+  });
 
   @override
-  List<Object> get props => [name];
+  List<Object> get props => [id];
 
   @override
   bool get stringify => true;
