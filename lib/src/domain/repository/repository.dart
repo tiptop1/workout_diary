ec01c420efb3f9af2a5272d985bc2c0268b7bcc1
tiptop <Tomasz.Gasior@gmail.com>
2023-12-19 22:34:01 +0100

    Change API to take into account synthetic keys of business entities.

--- lib/src/domain/repository/repository.dart
+++ lib/src/domain/repository/repository.dart
@@ -1,27 +1,34 @@
-import 'package:dartz/dartz.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:workout_diary/src/domain/entity/exercise_set.dart';
 
 import '../../common/failures.dart';
 import '../entity/exercise.dart';
 import '../entity/workout.dart';
 
 abstract class Repository {
+  Future<Either<Failure, ExerciseId>> nextExerciseId();
+
   Future<Either<Failure, List<Exercise>>> getAllExercises();
 
-  Future<Either<Failure, Exercise>> getExercise(String name);
+  Future<Either<Failure, Exercise>> getExercise(ExerciseId id);
 
   Future<Either<Failure, void>> addExercise(Exercise exercise);
 
-  Future<Either<Failure, void>> removeExercise(String name);
+  Future<Either<Failure, void>> removeExercise(Exercise exercise);
 
   Future<Either<Failure, void>> modifyExercise(Exercise exercise);
 
+  Future<Either<Failure, WorkoutId>> nextWorkoutId();
+
   Future<Either<Failure, List<Workout>>> getAllWorkouts();
 
-  Future<Either<Failure, Workout>> getWorkout(DateTime startTime);
+  Future<Either<Failure, Workout>> getWorkout(WorkoutId id);
 
   Future<Either<Failure, void>> addWorkout(Workout workout);
 
-  Future<Either<Failure, void>> removeWorkout(DateTime startTime);
+  Future<Either<Failure, void>> removeWorkout(Workout workout);
 
   Future<Either<Failure, void>> modifyWorkout(Workout workout);
+
+  Future<Either<Failure, ExerciseSetId>> nextExerciseSetId();
 }
