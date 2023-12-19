1262eae5a82972a6e171ab5dd568d04b1663a14f
tiptop <Tomasz.Gasior@gmail.com>
2023-12-19 22:37:06 +0100

    Remove index on natural key. Make id required.

--- lib/src/data/db/model/exercise_model.dart
+++ lib/src/data/db/model/exercise_model.dart
@@ -1,22 +1,20 @@
 import 'package:equatable/equatable.dart';
 import 'package:floor/floor.dart';
 
-/// Natural key: [name]
 @Entity(tableName: 'exercises')
-@Index(name: 'UX_Exercise_name', value: ['name'])
 class ExerciseModel extends Equatable {
-  @PrimaryKey(autoGenerate: true)
-  final int? id;
+  @primaryKey
+  final int id;
   final String name;
   final String? description;
 
   const ExerciseModel({
-    this.id,
+    required this.id,
     required this.name,
     this.description,
   });
 
   @override
-  List<Object?> get props => [id, name];
+  List<Object?> get props => [id];
 
 }
