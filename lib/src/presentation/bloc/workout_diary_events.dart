38580d1ddde8a6f5c94628ee21b7130bf9d9ae2a
tiptop <Tomasz.Gasior@gmail.com>
2024-01-07 18:18:44 +0100

    Rename super class - was: Event, is: WorkoutDiaryEvent. Add ShowMainPage event.

--- lib/src/presentation/bloc/workout_diary_events.dart
+++ lib/src/presentation/bloc/workout_diary_events.dart
@@ -1,66 +1,69 @@
-import 'package:equatable/equatable.dart';
-
 import '../../domain/entity/exercise.dart';
 import '../../domain/entity/workout.dart';
 
-abstract class Event extends Equatable {
-  const Event();
-
-  @override
-  List<Object?> get props => [];
+abstract class WorkoutDiaryEvent {
+  const WorkoutDiaryEvent();
 }
 
-class GetAllExercises extends Event {
+class LoadData extends WorkoutDiaryEvent {
+  const LoadData();
+}
+
+class ShowMainPage extends WorkoutDiaryEvent {
+  const ShowMainPage();
+}
+
+class GetAllExercises extends WorkoutDiaryEvent {
   const GetAllExercises();
 }
 
-class GetExercise extends Event {
+class GetExercise extends WorkoutDiaryEvent {
   final ExerciseId exerciseId;
 
   const GetExercise(this.exerciseId);
 }
 
-class AddExercise extends Event {
+class AddExercise extends WorkoutDiaryEvent {
   final Exercise exercise;
 
   const AddExercise(this.exercise);
 }
 
-class ModifyExercise extends Event {
+class ModifyExercise extends WorkoutDiaryEvent {
   final Exercise exercise;
 
   const ModifyExercise(this.exercise);
 }
 
-class RemoveExercise extends Event {
+class RemoveExercise extends WorkoutDiaryEvent {
   final Exercise exercise;
 
   const RemoveExercise(this.exercise);
 }
 
-class GetAllWorkouts extends Event {
+class GetAllWorkouts extends WorkoutDiaryEvent {
   const GetAllWorkouts();
 }
 
-class GetWorkout extends Event {
+class GetWorkout extends WorkoutDiaryEvent {
   final WorkoutId workoutId;
 
   const GetWorkout(this.workoutId);
 }
 
-class AddWorkout extends Event {
+class AddWorkout extends WorkoutDiaryEvent {
   final Workout workout;
 
   const AddWorkout(this.workout);
 }
 
-class ModifyWorkout extends Event {
+class ModifyWorkout extends WorkoutDiaryEvent {
   final Workout workout;
 
   const ModifyWorkout(this.workout);
 }
 
-class RemoveWorkout extends Event {
+class RemoveWorkout extends WorkoutDiaryEvent {
   final Workout workout;
 
   const RemoveWorkout(this.workout);
