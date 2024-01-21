b70ff1aa7940091452b61b42ba423ee475d97ef8
tiptop <Tomasz.Gasior@gmail.com>
2024-01-21 23:13:10 +0100

    Refactor event - remain needed at the moment.

--- lib/src/presentation/bloc/workout_diary_events.dart
+++ lib/src/presentation/bloc/workout_diary_events.dart
@@ -5,66 +5,31 @@ abstract class WorkoutDiaryEvent {
   const WorkoutDiaryEvent();
 }
 
-class LoadData extends WorkoutDiaryEvent {
-  const LoadData();
+class ShowMainRouteEvent extends WorkoutDiaryEvent {
+  const ShowMainRouteEvent();
 }
 
-class ShowMainPage extends WorkoutDiaryEvent {
-  const ShowMainPage();
+class AddExerciseEvent extends WorkoutDiaryEvent {
+  final String name;
+  final String? description;
+
+  const AddExerciseEvent(this.name, this.description);
 }
 
-class GetAllExercises extends WorkoutDiaryEvent {
-  const GetAllExercises();
-}
-
-class GetExercise extends WorkoutDiaryEvent {
-  final ExerciseId exerciseId;
-
-  const GetExercise(this.exerciseId);
-}
-
-class AddExercise extends WorkoutDiaryEvent {
+class ModifyExerciseEvent extends WorkoutDiaryEvent {
   final Exercise exercise;
 
-  const AddExercise(this.exercise);
+  const ModifyExerciseEvent(this.exercise);
 }
 
-class ModifyExercise extends WorkoutDiaryEvent {
+class DeleteExerciseEvent extends WorkoutDiaryEvent {
   final Exercise exercise;
 
-  const ModifyExercise(this.exercise);
+  const DeleteExerciseEvent(this.exercise);
 }
 
-class RemoveExercise extends WorkoutDiaryEvent {
-  final Exercise exercise;
-
-  const RemoveExercise(this.exercise);
-}
-
-class GetAllWorkouts extends WorkoutDiaryEvent {
-  const GetAllWorkouts();
-}
-
-class GetWorkout extends WorkoutDiaryEvent {
-  final WorkoutId workoutId;
-
-  const GetWorkout(this.workoutId);
-}
-
-class AddWorkout extends WorkoutDiaryEvent {
+class DeleteWorkoutEvent extends WorkoutDiaryEvent {
   final Workout workout;
 
-  const AddWorkout(this.workout);
-}
-
-class ModifyWorkout extends WorkoutDiaryEvent {
-  final Workout workout;
-
-  const ModifyWorkout(this.workout);
-}
-
-class RemoveWorkout extends WorkoutDiaryEvent {
-  final Workout workout;
-
-  const RemoveWorkout(this.workout);
+  const DeleteWorkoutEvent(this.workout);
 }
