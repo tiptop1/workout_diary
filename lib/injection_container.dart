4882342d6ed8acd4ec941d895dcf58ba8f8ce712
tiptop <Tomasz.Gasior@gmail.com>
2024-01-21 23:00:12 +0100

    Initial commit - injection container build on GetIt library.

---
+++ lib/injection_container.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+import 'package:workout_diary/src/data/db/local_repository.dart';
+import 'package:workout_diary/src/data/db/workout_diary_database.dart';
+
+import 'src/domain/use_case/exercise_use_cases.dart';
+import 'src/domain/use_case/workout_use_cases.dart';
+
+void init() async {
+  final db = await $FloorWorkoutDiaryDatabase.databaseBuilder('app_database.db').build();
+  final repo = LocalRepository(db);
+  GetIt.I.registerLazySingleton(() => ExerciseUseCases(repo));
+  GetIt.I.registerLazySingleton(() => WorkoutUseCases(repo));
+}
+
+
