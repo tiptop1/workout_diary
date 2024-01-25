399dee99a9ebaf1e318d8378914a98e28ef81f55
tiptop <Tomasz.Gasior@gmail.com>
2024-01-25 22:06:31 +0100

    Return Future<void> instead of void - it's asynchronous code block.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,7 +5,7 @@ import 'package:workout_diary/src/data/db/workout_diary_database.dart';
 import 'src/domain/use_case/exercise_use_cases.dart';
 import 'src/domain/use_case/workout_use_cases.dart';
 
-void init() async {
+Future<void> init() async {
   final db = await $FloorWorkoutDiaryDatabase.databaseBuilder('app_database.db').build();
   final repo = LocalRepository(db);
   GetIt.I.registerLazySingleton(() => ExerciseUseCases(repo));
